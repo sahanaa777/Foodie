6295ebec87d34bb385221c0a85434fe5a80e7f5c
sahanaa777 <128054418+sahanaa777@users.noreply.github.com>
2024-01-04 08:32:07 +0530

    Add files via upload

---
+++ fd.js
@@ -0,0 +1,70 @@
+function bh(){
+    document.getElementById('aa').src="pics/breadhalwa.mp4";
+    document.getElementById('2').innerHTML="Bread Halwa";
+    document.getElementById('3').innerHTML="Recipe<br>- Take a few bread slices and cut of the crusts<br>- To a kadai add oil and fry the bread<br>- Keep string the bread and remove the bread from the oil once it has turned light golden brown.<br>- Next take a pan and heat 4-5 tablespoons of ghee and keep it on medium to low heat<br>- Add 2 tablespoons of cashew to the pan and keep stirring<br>- After a few minutes add 2 tbsp of raisin to the same pan.<br>- Fry until the cashew turn golden brown and raisin swell up<br>- Remove both and keep it the side to cool down<br>Sugar syrup<br>- Add 7-8 tbsp of sugar ( if the halwa is made with sweet bread use less sugar)<br>- Add 1 and 1/2 cups of water and bring the mixture to a boil. * Remember to keep stirring on medium to low heat.<br>- Add the golden brown bread cubes to the syrup and keep stirring<br>- Once the mixture has thickened add 1/2 teaspoon cardamom powder to it and mix well.<br>* Garnish the bread halwa with golden fried cashew and raisin to it and keep stirring";
+}
+function cd(){
+    document.getElementById('aa').src="pics/carrotdosa.mp4";
+    document.getElementById('2').innerHTML="Carrot Dosa";
+    document.getElementById('3').innerHTML="Carrot Dosa recipe<br>Dosa bater - 3 cups<br>Grated carrots - 3<br>Onion - 1<br>Jeera - 1 tsp<br>Ginger - 1 small piece<br>SALT as needed.<br>Red chilli - 3<br>Grind all ingredients to a thick paste<br>add the paste to the dosa batter and mix well<br>add finely chapped curry leaves and coriander<br>leaves - (optional)<br>";
+}
+function cb(){
+    document.getElementById('aa').src="pics/chillibajji.mp4";
+    document.getElementById('2').innerHTML="Chilli Bajji";
+    document.getElementById('3').innerHTML="Recipe :<br> - Roast and grind bengal gram (1/4 cup) Urad dal 2 Tbsp together<br> - To the mixture add 1/2 tsp chilli powder<br> - 1/2 tsp coriander powder<br>- 1/2 Tsp jeera powder<br>    - Asafoetida 1/2 tsp<br>    - Pinch of turmeric<br>    - 1/2 tsp sugar<br>    - Salt to taste<br>    - Slight drizzle of oil and mix everything together.<br> - Slit the chilli in half and add the mixture<br>    - Shallow fry until it changes to a light green colour And enjoy 😄";
+}
+function fc(){
+    document.getElementById('aa').src="pics/coffee.mp4";
+    document.getElementById('2').innerHTML="Filter Coffee";
+    document.getElementById('3').innerHTML="Ingredients<br>Concentrated filter coffee - 1/2 cup<br>Coconut milk - 3/4 cup<br>Honey as required<br>Vanilla essence - 1 drop<br>Unsweetened Cocoa Powder - 2 tbsp<br>Salt - a small pinch<br>Chia seeds - 1/2 cup<br>";
+}
+function eb(){
+    document.getElementById('aa').src="pics/energybar.mp4";
+    document.getElementById('2').innerHTML="Dates's Energy Bar";
+    document.getElementById('3').innerHTML="Recipe<br>Soak 1 cup dates in 2 cup of water hot water for 1 - 2 hours<br>Blend and make a paste<br>Add 1 cup of all your favourite nuts onto a pan<br>Stir until it’s Golden brown<br>Add the paste to a pan on low heat, stir until it thickens<br>Add the the nuts and mix well<br>Add honey/syrup and mix well<br>Add half a cup of finely grinded Oats<br>Mix everything and make sure they combine together<br>Spread it evenly on a tray or bowl<br>Let it set in the fridge for about 2 hours.<br>Your energy bar should be ready to eat<br> Cut into small pieces and serve!<br>";
+}
+function ld(){
+    document.getElementById('aa').src="pics/laddoo.mp4";
+    document.getElementById('2').innerHTML="Laddoo";
+    document.getElementById('3').innerHTML="Recipe<br>In a food processor<br>Add 1 cup dates<br>3 tbsp shredded coconut<br>1 or 2 tbsp flax seeds<br>1 tbsp of cashew<br>1 tbsp walnut<br>1 tbsp almond<br>1 tbsp pistachio<br>1 tbsp raisin<br>1 tbsp poppy seeds<br>1/2 tsp of cardamom powder<br>Blend well<br>Add 1 tsp of ghee<br>Blend once again.<br>Make sure all ingredients are around the same size before moving on to the next step.<br>Roll them by hand into small Laddoos and you’re good to go.<br>A really easy and quick recipe that really take less than 5 mins.<br>";
+}
+function lde(){
+    document.getElementById('aa').src="pics/lemonade.mp4";
+    document.getElementById('2').innerHTML="Lemonade";
+    document.getElementById('3').innerHTML="Recipe<br>2 lime’s<br>3 cups water<br>½ cup sugar<br>3 tablespoons sweetened condensed milk<br>ice cubes<br>Tip : Make sure you add a lot of condensed milk 😄";
+}
+function mg(){
+    document.getElementById('aa').src="pics/maggi.mp4";
+    document.getElementById('2').innerHTML="Maggi Ramen";
+    document.getElementById('3').innerHTML="Ingredients :<br>5 cloves garlic<br>1 small piece ginger<br>2 tsp olive oil<br>150-200 g Mushrooms, chopped<br>2 tbsp peanut butter<br>1-2 cups vegetable stock ( based on how soupy you want your ramen to be )<br>1 portion ramen noodles<br>1 tbsp cane sugar<br>1 tsp rice vinegar<br>2 tbsp soy sauce<br>1 tsp sesame oil<br>Disclaimer: the above is the original recipe that has been adapted and changed for the purpose of the video. (Egg and broccoli added)";
+}
+function mp(){
+    document.getElementById('aa').src="pics/masalapuri.mp4";
+    document.getElementById('2').innerHTML="Masala Poori";
+    document.getElementById('3').innerHTML="Recipe and ingredients<br>1 cup Rava, 1 cup water<br>Mix together and rest for 5 mins<br>1 chopped green chilli, 3tbsp chopped coriander, 1tbsp chilli flakes<br>Salt to taste, 1 teaspoon cumin seeds, 1 Boiled medium size potato<br>1 cup wheat flour<br><br>Mix them all tougher to prepare your dough.Rest for 10 mins<br>Grease with oil and make small puris and fry them in oil.<br>You can have this with raitha, or even a channa masala";
+}
+function mm(){
+    document.getElementById('aa').src="pics/mogumogu.mp4";
+    document.getElementById('2').innerHTML="Mogu Mogu";
+    document.getElementById('3').innerHTML="Recipe<br>200 g Coconut Water<br>1/2 tsp Agar Agar powder<br>1 big bowl of Lychees<br>1 Glass Water<br>A little Sugar<br>Juice of 1 Lime<br>Beetroot Puree for a pink colour";
+}
+function pp(){
+    document.getElementById('aa').src="pics/panipuri.mp4";
+    document.getElementById('2').innerHTML="Pani Puri";
+    document.getElementById('3').innerHTML="Ingredients;<br>For the spicy sweet paani 🌶️<br></br>    1. Fresh mint leaves - 1 cup<br>2. Fresh coriander leaves - 1 cup<br>3. Green chilies- 3 nos.<br>4. Ginger - 1 inch<br>5. Salt - as per taste<br>6. Chat masala - 1 tsp<br>7. Cumin powder - 1 tsp<br>8. Amchur powder - 1 tsp<br>9. Tamarind water - 1/2 cup<br>Potato filling 🥔<br></br>1. Boiled potato - coarsely mashed<br>2. Soaked Boondi - 1/2 cup<br>3. Finely chopped onions - 1/4 cup<br>4. Red chilli powder - 1/2 tsp<br> 5. Lemon juice - 1 tsp";
+}
+function pi(){
+    document.getElementById('aa').src="pics/pudiidli.mp4";
+    document.getElementById('2').innerHTML="Pudi Idli";
+    document.getElementById('3').innerHTML="Ingredients to make home made Pudi Idli<br>1 tablespoon Chana Dal<br>1 Urad dal<br>3 dried red chilies<br>1 1/2 teaspoons sesame seeds<br>1 sprig of curry leaves<br>1/4 teaspoon Salt<br>½ teaspoon asafoetida (hing)<br>3 plates of mini idles<br>2 to 3 tablespoons of Ghee";
+}
+function mpu(){
+    document.getElementById('aa').src="pics/pulav.mp4";
+    document.getElementById('2').innerHTML="Masala Pulav";
+    document.getElementById('3').innerHTML="Recipe<br>Clean, wash and soak the rice for 30 mins.<br>Heat a deep bottomed pan and melt ghee, add cashew nuts, raisins and stir them cook till they brown up a little.<br>Remove them and keep aside.<br>In the same ghee add bayleaf, cinnamon, cloves, black cardamom, star anise, and fennel seeds.<br>Stir for few seconds and then add in onions. Cook the onions till they brown. At this stage you can remove a spoon of browned/fried onions to put them for garnish later.<br>Put in the ginger paste and stir again.<br>Pour water, salt and green chillies.<br>Bring the water to a boil, in the mean while drain the water from the rice and add them to the boiling water.<br>Add the cashew nuts and raisins back to the rice.<br>Cook the rice till the water level reduces till the level of the rice in the pan, lower the heat, cover and cook till all of the water is absorbed.<br>Turn off the heat and let it sir for 15mins covered.<br>Remove the lid and carefully scoop out the ghee rice and serve with a curry of your choice or with plain curd.<br>(The veggies we added in the video are optional and can be added if you prefer it that way )";
+}
+function mt(){
+    document.getElementById('aa').src="pics/tea.mp4";
+    document.getElementById('2').innerHTML="Masala Tea";
+    document.getElementById('3').innerHTML="Recipe<br>Special tea enhanced with the Bombay chai flavour and Hyderabad Irani flavour. If you’re a tea lover then I can assure you that this is for you. ";
+}
